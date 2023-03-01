ba91ac4467e11988d0d3f34a911c2ab919e3ad8b
student <Jeff Takaki>
2023-03-01 12:14:57 -0600

    added elastic ip to webserver

--- infrastructure/create-stacks.sh
+++ infrastructure/create-stacks.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 aws cloudformation deploy --stack-name book-management-vpc --template-file ./vpc.yaml --region us-east-1 --no-fail-on-empty-changeset
+aws cloudformation deploy --stack-name book-management-ip --template-file ./ip.yaml --region us-east-1 --no-fail-on-empty-changeset
 aws cloudformation deploy --stack-name book-management-security --template-file ./security.yaml --capabilities CAPABILITY_IAM --region us-east-1 --no-fail-on-empty-changeset
 aws cloudformation deploy --stack-name book-management-web --template-file ./webserver.yaml --region us-east-1 --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
