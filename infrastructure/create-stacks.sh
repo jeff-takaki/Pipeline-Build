8fe007bc0144a354be7933f190aee47a12ceae0c
student <Jeff Takaki>
2023-04-24 14:52:30 -0500

    Adding Blue Green Zero Downtime Deployment Config

--- infrastructure/create-stacks.sh
+++ infrastructure/create-stacks.sh
@@ -2,5 +2,5 @@
 aws cloudformation deploy --stack-name book-management-vpc --template-file ./vpc.yaml --region us-east-1 --no-fail-on-empty-changeset
 aws cloudformation deploy --stack-name book-management-ip --template-file ./ip.yaml --region us-east-1 --no-fail-on-empty-changeset
 aws cloudformation deploy --stack-name book-management-security --template-file ./security.yaml --capabilities CAPABILITY_IAM --region us-east-1 --no-fail-on-empty-changeset
-aws cloudformation deploy --stack-name book-management-web --template-file ./webserver.yaml --region us-east-1 --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
-aws cloudformation deploy --stack-name book-management-db --template-file ./db.yaml --region us-east-1 --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+aws cloudformation deploy --stack-name book-management-web-dev --template-file ./webserver-bg.yaml --region us-east-1 --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+#aws cloudformation deploy --stack-name book-management-db --template-file ./db.yaml --region us-east-1 --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
